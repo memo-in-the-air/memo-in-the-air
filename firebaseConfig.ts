b6d5dc8096b59827b4a6fadc405b5eb1da479c0d
HANSOL <hansol302@gmail.com>
2025-04-11 11:46:22 +0900

    Feat: 자동로그인을 위하여 Firebas Auth에 대한 설정 추가

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -1,5 +1,6 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { getReactNativePersistence, initializeAuth } from "firebase/auth";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
@@ -14,7 +15,9 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(AsyncStorage),
+});
 const database = getDatabase(app);
-const auth = getAuth(app);
 
 export { database, auth };
